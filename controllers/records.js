e62f7a0472465fd43db1aedd56cac7f66999ef1c
jwcrosby <jwcrosby89@gmail.com>
2021-07-18 18:52:01 -0600

    add delete button and minor style tweaks

--- controllers/records.js
+++ controllers/records.js
@@ -1,7 +1,18 @@
 import { Record } from '../models/record.js'
 
 export {
+    newRecord as new,
     show
+}
+
+function newRecord(req, res) {
+
+    Record.find({}, function(err, records) {
+        res.render("records/new", {
+            title: "Add Record",
+            records: records,
+        })
+    })
 }
 
 function show(req, res) {
