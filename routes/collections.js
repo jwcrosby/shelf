59b7e7cdc97e5a7fedfb1d5399e965a6dfab55a3
jwcrosby <jwcrosby89@gmail.com>
2021-07-18 20:35:32 -0600

    add new records route and view

--- routes/collections.js
+++ routes/collections.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import * as collectionsCtrl from "../controllers/collections.js"
+import * as recordsCtrl from "../controllers/records.js"
 import { isLoggedIn } from '../middleware/middleware.js'
 
 export {
@@ -14,4 +15,8 @@ router.post("/", isLoggedIn, collectionsCtrl.create)
 
 router.get("/:collectionId", collectionsCtrl.show)
 
-router.delete("/:collectionId", collectionsCtrl.delete)
+router.delete("/:collectionId", isLoggedIn, collectionsCtrl.delete)
+
+//?Records routes
+
+router.get('/:collectionId/new-record/', recordsCtrl.new)
