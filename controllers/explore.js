996aa45b43983844d42f726a1086715eb6da5929
jwcrosby <jwcrosby89@gmail.com>
2021-07-23 10:10:10 -0600

    update explore show route

--- controllers/explore.js
+++ controllers/explore.js
@@ -24,29 +24,56 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    Profile.findById(req.params.id)
-    .then(profile => {
-        Profile.findById(req.user.profile._id)
-        .then(self => {
 
-            const isSelf = self._id.equals(profile._id)
+    if(req.user) {
+            Profile.findById(req.user.profile._id)
+            .then(self => {
+                console.log(self._id)
+                console.log("test")
+                console.log(req.params.profileId)
+                console.log("test")
+    
+                const isSelf = self._id.equals(req.params.profileId) ? req.params.profileId : null
 
-            Collection.findById(req.params.collectionId)
-            .populate("records")
-            .populate("owner")
-            
-            .then(collection => {
-                res.render('collections/show', {
-                collection,
-                title: `${collection.name} | Shelf`,
-                self,
-                isSelf
+                console.log()
+                Collection.findById(req.params.collectionId)
+                .populate("records")
+                .populate("owner")
+                
+                .then(collection => {
+                    res.render('explore/show', {
+                    collection,
+                    title: `${collection.name} | Shelf`,
+                    self,
+                    isSelf
+                    })
+                })
+                .catch(err => {
+                    console.log(err)
+                    res.redirect('/error')
                 })
             })
-            .catch(err => {
-                console.log(err)
-                res.redirect('/error')
+
+    } else {
+
+        const isSelf = null
+
+        Collection.findById(req.params.collectionId)
+        .populate("records")
+        .populate("owner")
+        
+
+        .then(collection => {
+            res.render('explore/show', {
+            collection,
+            title: `${collection.name} | Shelf`,
+            self: null,
+            isSelf
             })
         })
-    })
+        .catch(err => {
+            console.log(err)
+            res.redirect('/error')
+        })
+    }
 }
